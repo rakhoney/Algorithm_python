ff32925c2a746ec51d6bc8b18530a4a19944b694
rakhoney <126341802+rakhoney@users.noreply.github.com>
2024-05-13 03:10:24 +0900

    [Bronze II] Title: 나머지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,6 +1,15 @@
+## 1. set함수로 풀기
 num = []
 for _ in range(10):
     num.append(int(input())%42)
 
 anw = set(num)
 print(len(anw))
+
+# ## 2. for문으로 풀기
+# num = []
+# for _ in range(10):
+#     a = int(input())
+#     if a%42 not in num:
+#         num.append(a%42)
+# print(len(num))
